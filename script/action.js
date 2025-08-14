ce1d12af59dfd508a3839edc98be30b4a3ced9c5
grace-j-25 <0824824@naver.com>
2025-08-14 17:01:17 +0900

    화면확장시 모바일메뉴 사라지는 js코드 추가

--- script/action.js
+++ script/action.js
@@ -26,5 +26,12 @@ $('.mobile_nav .gnb > li').click(function(e){
     $('.mobile_nav .gnb .lnb a').click(function(e){
         e.stopPropagation(); // 부모 li로 이벤트 전달 안 함
         // preventDefault() 안 씀 → 링크 정상 이동
+    });
 });
+
+$(window).resize(function(){
+    if ($(window).width() > 1024) { // 데스크탑 전환 기준
+        $('.mobile_nav').removeAttr('style'); // display:block 등 inline style 제거
+        $('.mobile_nav .lnb').removeAttr('style'); // 2차 메뉴들도 닫힘 상태로
+    }
 });
